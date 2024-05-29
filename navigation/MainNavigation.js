c12b97dcdb9d15d60c191e2406090567cd7ed235
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-29 23:30:54 +0200

    created the MainNavigation.js

---
+++ navigation/MainNavigation.js
@@ -0,0 +1,11 @@
+import { createStackNavigator } from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const MainNavigation = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name={} component={ } />
+        </Stack.Navigator>
+    )
+}
