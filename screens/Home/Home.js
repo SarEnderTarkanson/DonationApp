64331cf3d57ddd0514ddcdacdbd0eeadd72d7928
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-30 01:48:45 +0200

    created a button component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import Header from '../../components/Header/Header';
+import Button from '../../components/Button/Button';
 import globalStyle from '../../assets/styles/globalStyle';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Sare Beyza'} type={1} />
-      <Header title={'Sare Beyza'} type={2} />
-      <Header title={'Sare Beyza'} type={3} />
+      <Button
+        title="Donate"
+        onPress={() => {
+          console.log('you just pressed me');
+        }}
+      />
+      <Button title="Donate" isDisabled={true} />
     </SafeAreaView>
   );
 };
