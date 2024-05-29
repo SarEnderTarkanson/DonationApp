f7a5b2020225e9bbd054ce25bf6d0641b29567b6
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-29 23:53:28 +0200

    added styles, routes, home component

--- navigation/MainNavigation.js
+++ navigation/MainNavigation.js
@@ -1,11 +1,15 @@
-import { createStackNavigator } from "@react-navigation/stack";
+import {createStackNavigator} from '@react-navigation/stack';
+import {Routes} from './Routes';
+import Home from '../screens/Home/Home';
 
 const Stack = createStackNavigator();
 
 const MainNavigation = () => {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name={} component={ } />
-        </Stack.Navigator>
-    )
-}
+  return (
+    <Stack.Navigator screenOptions={{header: () => null, headerShown: false}}>
+      <Stack.Screen name={Routes.Home} component={Home} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigation;
---
+++ screens/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
+import globalStyle from '../../assets/styles/globalStyle';
+import style from './style';
+
+const Home = () => {
+  return (
+    <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
+      <View>
+        <Text>Hello World</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Home;
