600749618253a3cfb6e786f2339df4cd79dfc010
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-05-30 02:07:20 +0200

    added the Tab component and its styles

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -2,19 +2,15 @@ import React from 'react';
 import {SafeAreaView, Text, View} from 'react-native';
 import Header from '../../components/Header/Header';
 import Button from '../../components/Button/Button';
+import Tab from '../../components/Tab/Tab';
 import globalStyle from '../../assets/styles/globalStyle';
 
 const Home = () => {
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={'Sare Beyza'} type={1} />
-      <Button
-        title="Donate"
-        onPress={() => {
-          console.log('you just pressed me');
-        }}
-      />
-      <Button title="Donate" isDisabled={true} />
+      <Tab title={'Highlight'} />
+      <Tab title={'Highlight'} isInactive={true} />
     </SafeAreaView>
   );
 };
