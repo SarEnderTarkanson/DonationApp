c338d34d64b1a7a832c2f1f9d474b2589a123b66
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-02 03:24:25 +0200

    update Home component

--- screens/Home/Home.js
+++ screens/Home/Home.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {SafeAreaView} from 'react-native';
+import {Pressable, SafeAreaView, Text} from 'react-native';
 import Header from '../../components/Header/Header';
 import globalStyle from '../../assets/styles/globalStyle';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
+import {updateFirstName} from '../../redux/reducers/User';
 
 const Home = () => {
   const user = useSelector(state => state.user);
+  const dispatch = useDispatch();
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
       <Header title={user.firstName + ' ' + user.lastName} />
+      <Pressable onPress={() => dispatch(updateFirstName({firstName: 'A'}))}>
+        <Text>Press me to change first name</Text>
+      </Pressable>
     </SafeAreaView>
   );
 };
