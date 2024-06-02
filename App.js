2641c37fca3cc77e5415e73bd138e033c143f0f3
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-02 03:48:22 +0200

    added persistor

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import MainNavigation from './navigation/MainNavigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import store from './redux/store';
+import {PersistGate} from 'redux-persist/integration/react';
+import {persistor} from './redux/store';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <MainNavigation />
-      </NavigationContainer>
+      <PersistGate persistor={persistor} loading={null}>
+        <NavigationContainer>
+          <MainNavigation />
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 };
