2c39d826d12250d0d56d538c743e5d2708aa8ea6
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-07 02:30:00 +0200

    Added a BackButton and applied styles to it

--- screens/SingleDonationItem/style.js
+++ screens/SingleDonationItem/style.js
@@ -1,7 +1,11 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
+import {horizontalScale, verticalScale} from '../../assets/styles/scaling';
 
 const style = StyleSheet.create({
+  container: {
+    marginHorizontal: horizontalScale(20),
+    marginTop: verticalScale(7),
+  },
+});
 
-})
-
-export default style
+export default style;
