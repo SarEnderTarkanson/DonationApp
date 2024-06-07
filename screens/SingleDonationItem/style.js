abe6ed31ee4c8415e2c76ec060412a13e38e9400
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2024-06-07 03:02:53 +0200

    added image, description, name and donate button the SingleDonationItem component

--- screens/SingleDonationItem/style.js
+++ screens/SingleDonationItem/style.js
@@ -1,11 +1,31 @@
 import {StyleSheet} from 'react-native';
 import {horizontalScale, verticalScale} from '../../assets/styles/scaling';
+import { getFontFamily } from '../../assets/fonts/helper';
 
 const style = StyleSheet.create({
   container: {
     marginHorizontal: horizontalScale(20),
     marginTop: verticalScale(7),
   },
+  image: {
+    marginTop: verticalScale(20),
+    marginBottom: verticalScale(24),
+    width: '100%',
+    height: verticalScale(240),
+    borderRadius: horizontalScale(5),
+  },
+  badge: {
+    marginBottom: verticalScale(16)
+  },
+  description: {
+    marginTop: verticalScale(7),
+    marginHorizontal: horizontalScale(7),
+    fontFamily: getFontFamily('Inter', '600'),
+    marginBottom: verticalScale(10)
+  },
+  button: {
+    marginHorizontal: horizontalScale(20)
+  }
 });
 
 export default style;
